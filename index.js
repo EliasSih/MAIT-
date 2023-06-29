140bee64d0cde91f5eaec97dbf3563a8eddc7229
EliasSih <eliazsih@gmail.com>
2023-06-29 13:54:12 +0200

    inserting the first five prefixes in each ASN to the database

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const ip = require('ip');
 const { exec } = require('child_process');
 const util = require('util');
 
+const neo4j = require('neo4j-driver');
+const driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "eli@sol2"));
 
 // Section : simple traceroute request to ripe atlas
 // from one probe to destination ip
@@ -168,26 +170,48 @@ async function geoLookup(ipAddress) {
     }
 }
 
-// Get ASN information
-// Define the async function
+// Assuming you have initiated Neo4j driver somewhere above in the code
+// const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+
 async function getAsnInfo(asn) {
     const url = `https://stat.ripe.net/data/announced-prefixes/data.json?resource=AS${asn}`;
 
     try {
         const response = await axios.get(url);
-        const prefixes = response.data.data.prefixes
+        let prefixes = response.data.data.prefixes
             .map(p => p.prefix)
             .filter(p => !p.includes(':'));  // Ignore IPv6 prefixes
+
+        // Limit the prefixes to only the first 5
+        prefixes = prefixes.slice(0, 5);
 
         // Only print the AS Number and prefixes if there are any prefixes
         if (prefixes.length > 0) {
             console.log(`ASN: AS${asn}`);
             console.log('Prefixes:', prefixes);
+
+            // Create a session to interact with the database
+            const session = driver.session({database: 'neo4j'});
+
+            // Iterate over each prefix
+            for (let prefix of prefixes) {
+                // Write the ASN and prefix into Neo4j
+                await session.run(
+                    `MERGE (a:Asn {asn: $asn})
+                     MERGE (p:Prefix {prefix: $prefix})
+                     MERGE (a)-[:HAS]->(p)`,
+                    {asn: `AS${asn}`, prefix: prefix}
+                );
+            }
+
+            // Close the session
+            session.close();
         }
     } catch (error) {
         console.error(`Error: ${error}`);
     }
 }
+
 
 // Load ASNs from a local file
 async function getPrefixes(filepath) {
@@ -203,6 +227,9 @@ async function getPrefixes(filepath) {
     const asn = line.split(' ')[0]; // Assumes the ASN is the first element on the line
     await getAsnInfo(asn);
   }
+
+  // Close the driver connection when you're finished
+  driver.close();
 }
 
 function getTracerouteTarget(prefix) {
