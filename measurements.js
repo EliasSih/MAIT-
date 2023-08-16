98e760ec379db1eec692ad8c966fea1159ef4784
EliasSih <eliazsih@gmail.com>
2023-08-16 04:32:19 +0200

    processing measurements in batches

--- measurements.js
+++ measurements.js
@@ -2,99 +2,119 @@ const axios = require('axios');
 const neo4j = require('neo4j-driver');
 const driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "eli@sol2"));
 
-async function createMeasurementFromDatabase(numberOfProbes, countryCode, probeIDs) {
+const SIMULTANEOUS_LIMIT = 100;
+const PROBES_PER_MEASUREMENT = 217;
+
+async function createMeasurementFromDatabase(numberOfProbes, numberOfPrefixes) {
     console.log("Measurement initiated!");
 
-    // Create a session to fetch probe data from the database
     const session = driver.session({database: 'neo4j'});
 
-    // Initialize an array to store the fetched probe IDs
-    let probes = [];
+    // Fetch the probes
+    let probeResult = await session.run(
+        `MATCH (p:Probe) RETURN p.id AS id LIMIT $limit`,
+        { limit: neo4j.int(numberOfProbes) }
+    );
+    let probes = probeResult.records.map(record => parseInt(record.get('id')));
 
-    if(probeIDs) {  // if specific probeIDs are given
-        probes = probeIDs;
-    } else {
-        let result;
-        if(countryCode) {  // if a countryCode is given
-            result = await session.run(
-                `MATCH (p:Probe {country: $countryCode}) RETURN p.id AS id LIMIT $limit`,
-                {countryCode: countryCode, limit: neo4j.int(numberOfProbes)}
-            );
-        } else {  // if no countryCode is given
-            result = await session.run(
-                `MATCH (p:Probe) RETURN p.id AS id LIMIT $limit`,
-                {limit: neo4j.int(numberOfProbes)}
-            );
-        }
-        // Extract the probe IDs from the result and add them to the probes array
-        result.records.forEach(record => probes.push(parseInt(record.get('id'))));
-    }
+    // Fetch prefixes
+    let prefixResult = await session.run(
+        `MATCH (p:Prefix)-[:HAS_REACHABLE_IP]->(t:Target)
+         RETURN DISTINCT p.prefix AS prefix
+         LIMIT $limit`,
+        { limit: neo4j.int(numberOfPrefixes) }
+    );
+    let prefixes = prefixResult.records.map(record => record.get('prefix'));
 
-    // Define the target IP
-    const target = '196.252.138.15';
-
-    // Define the API key
     const API_KEY = '216fafa6-2a2d-438a-9bb3-7309aa0acf59';
 
-    // Define the measurement data
-    const data = {
-        "definitions": [
-            {
-                "target": target,
-                "description": "Traceroute to " + target,
-                "type": "traceroute",
-                "is_oneoff": true,
-                "protocol": "ICMP",
-                "af": 4
-            }
-        ],
-        "probes": [
-            {
-                "requested": probes.length,
-                "type": "probes",
-                "value": probes.join(',')
-            }
-        ]
-    };
-
-    // Define the request configuration
-    const config = {
-        method: 'post',
-        url: 'https://atlas.ripe.net/api/v2/measurements/',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Key ${API_KEY}`
-        },
-        data: data
-    };
-
-    try {
-        const response = await axios(config);
-        console.log(JSON.stringify(response.data));
-
-        // Extract the measurement id from the response
-        let measurementId = response.data.measurements[0];
-
-        // Write to the database
-        await session.run(
-            `CREATE (m:Measurement {measurementId: $measurementId, isRead: false}) RETURN m`,
-            {measurementId: measurementId}
+    for (let prefix of prefixes) {
+        let targetResult = await session.run(
+            `MATCH (p:Prefix {prefix: $prefix})-[:HAS_REACHABLE_IP]->(t:Target)
+             WITH t, split(t.ip, '\n') AS ips
+             UNWIND ips AS ip
+             RETURN ip
+             ORDER BY rand()
+             LIMIT 2`,
+            { prefix: prefix }
         );
+        let targets = targetResult.records.map(record => record.get('ip'));
 
-        console.log("Write complete");
-    } catch (error) {
-        console.error(error);
-    } finally {
-        session.close();
-        console.log("Session closed");
-        driver.close();  // Close the driver connection
-        console.log("Driver closed");
+        for (let target of targets) {
+            // Process probes in batches for each target
+            for(let i = 0; i < probes.length; i += PROBES_PER_MEASUREMENT) {
+                let probeBatch = probes.slice(i, i + PROBES_PER_MEASUREMENT);
+
+                // Filter out probes that already have measurements for the target
+                let validProbes = [];
+                for(let probe of probeBatch) {
+                    let measurementCheck = await session.run(
+                        `MATCH (p:Probe {id: $probeId})-[:MEASURED]->(t:Target)
+                        WHERE t.ip CONTAINS $targetIp
+                        RETURN COUNT(*) as count`,
+                        { probeId: probe, targetIp: target }
+                    );
+                    if (measurementCheck.records[0].get('count').toNumber() == 0) {
+                        validProbes.push(probe);
+                    }
+                }
+
+                const data = {
+                    "definitions": [{
+                        "target": target,
+                        "description": "Traceroute to " + target,
+                        "type": "traceroute",
+                        "is_oneoff": true,
+                        "protocol": "ICMP",
+                        "af": 4
+                    }],
+                    "probes": [{
+                        "requested": validProbes.length,
+                        "type": "probes",
+                        "value": validProbes.join(',')
+                    }]
+                };
+
+                const config = {
+                    method: 'post',
+                    url: 'https://atlas.ripe.net/api/v2/measurements/',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Key ${API_KEY}`
+                    },
+                    data: data
+                };
+
+                try {
+                    const response = await axios(config);
+                    for(let probe of validProbes) {
+                        // Store the relationship
+                        await session.run(
+                            `MATCH (p:Probe {id: $probeId}), (t:Target)
+                             WHERE t.ip CONTAINS $targetIp
+                             MERGE (p)-[:MEASURED]->(t)`,
+                            { probeId: probe, targetIp: target }
+                        );
+                    }
+                    let measurementId = response.data.measurements[0];
+                    await session.run(
+                        `CREATE (m:Measurement {measurementId: $measurementId, isRead: false}) RETURN m`,
+                        {measurementId: measurementId}
+                    );
+                } catch (error) {
+                    console.error(error);
+                }
+
+                // Pause execution to respect the SIMULTANEOUS_LIMIT
+                await new Promise(resolve => setTimeout(resolve, 1000 * PROBES_PER_MEASUREMENT / SIMULTANEOUS_LIMIT));
+            }
+        }
     }
+
+    session.close();
+    driver.close();
 }
 
 module.exports.createMeasurementFromDatabase = createMeasurementFromDatabase;
 
-// Example usage:
-createMeasurementFromDatabase(5, 'ZA', null)
-    .then(() => console.log("Terminate!"))
-    .catch(err => console.error(err));
+createMeasurementFromDatabase(217, 5573).then(() => console.log("Terminate!"));
